18d5436b4dfbbbe3f0136280fad4c5b418ab6cb0
Cristian <cac@treming.com>
2023-05-09 16:04:06 -0600

    diseño nuevo del home amarrillo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
-function nt(){
-const carousel = document.querySelectorAll('.carousel-item');
-
-if(carousel.classList.add("active"))
-{
-    carousel.style.display = 'block';
-    carousel.style.opacity = 1;
-}
-else
-{
-    carousel.style.display = 'none';
-}
-}
-
-nt();
+$(document).ready(function(){
+   /* ---------------------Testimonial carousel------------------- */
+  $(".testimonials-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+  });
